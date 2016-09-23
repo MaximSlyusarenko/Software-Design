02d1deb03b784941155e4ca2150985c84dbfb96d
MaximSlyusarenko <slyuslyu.m@mail.ru>
2016-09-23 15:26:13 +0300

    Asserts added, some things fixed, not tested yet

--- Lab 1/src/main/java/LRUCache.java
+++ Lab 1/src/main/java/LRUCache.java
@@ -1,5 +1,6 @@
 import com.google.common.base.Preconditions;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
@@ -17,19 +18,28 @@ public class LRUCache<K, V> {
     public LRUCache(int capacity) {
         Preconditions.checkArgument(capacity > 0, "Capacity must be greater then zero");
         this.capacity = capacity;
+        this.keyToNode = new HashMap<>();
     }
 
     public Optional<V> get(K key) {
+        assert keyToNode.size() <= capacity : "Map size is greater than capacity";
         Node<K, V> node = keyToNode.get(key);
+        Optional<V> result;
         if (node == null) {
-            return Optional.empty();
+            result = Optional.empty();
         } else {
             becomeHead(node);
-            return Optional.of(node.value);
+            result = Optional.of(node.value);
         }
+        assert key == head.key : "Key didn't become head after get";
+        return result;
     }
 
     public void set(K key, V value) {
+        assert keyToNode.size() <= capacity : "Map size is greater than capacity";
+        Node<K, V> headBefore = new Node<K, V>(head);
+        Node<K, V> tailBefore = new Node<K, V>(tail);
+        boolean inserted = false;
         if (keyToNode.size() == 0 || (keyToNode.size() == capacity && capacity == 1)) {
             head = new Node<>(key, value);
         } else if (keyToNode.size() == capacity) {
@@ -37,12 +47,26 @@ public class LRUCache<K, V> {
             if (node == null) {
                 keyToNode.remove(tail.key);
                 tail.prev.next = null;
-                insert(key, value);
+                tail = tail.prev;
+                inserted = insert(key, value);
             } else {
                 becomeHead(node);
             }
         } else {
-            insert(key, value);
+            inserted = insert(key, value);
+        }
+        assert head.key.equals(key) : "Head key not equals to inserted key";
+        assert head.value.equals(value) : "Head value not equals to inserted value";
+        if (capacity == 1) {
+            assert tail == null : "Capacity is one, tail is not null";
+        }
+        if (inserted) {
+            assert tailBefore.key != tail.key && tailBefore.value != tail.value : "The map was full, but tail hasn't changed";
+        }
+        if (headBefore.key == key) {
+            assert head == headBefore : "Head has changed, but mustn't";
+        } else {
+            assert head.key != headBefore.key : "Head hasn't change but must";
         }
     }
 
@@ -53,18 +77,26 @@ public class LRUCache<K, V> {
         if (node.next != null) {
             node.next.prev = node.prev;
         }
+        if (node == tail) {
+            tail = tail.prev;
+        }
         head.prev = node;
         head = node;
     }
 
-    private void insert(K key, V value) {
+    private boolean insert(K key, V value) {
         Node<K, V> node = keyToNode.get(key);
         if (node == null) {
             node = new Node<>(key, value, null, head);
+            if (tail == null) {
+                tail = head;
+            }
             head = node;
             keyToNode.put(key, node);
+            return true;
         } else {
             becomeHead(node);
+            return false;
         }
     }
 
@@ -86,6 +118,13 @@ public class LRUCache<K, V> {
             this.value = value;
             this.prev = prev;
             this.next = next;
+        }
+
+        private Node(Node<NK, NV> node) {
+            this.key = node.key;
+            this.value = node.value;
+            this.prev = node.prev;
+            this.next = node.next;
         }
     }
 }
